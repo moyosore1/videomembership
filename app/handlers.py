858d7a4f8e20a7164e7e3b3bd494d106e89f52d0
moyosore1 <moyosoreolumideobi@gmail.com>
2022-04-09 12:03:57 -0400

    updates

--- app/handlers.py
+++ app/handlers.py
@@ -1,11 +1,18 @@
-from starlette.exceptions import HTTPException
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from app.main import main_app
-from app.shortcuts import render
+from app.shortcuts import redirect, render
+from app.users.exceptions import LoginRequiredException
 
-@main_app.exception_handler(HTTPException)
+
+@main_app.exception_handler(StarletteHTTPException)
 async def http_exception_handler(request, exc):
     status_code = exc.status_code
     template_name = "error.html"
     context = {"status_code":status_code}
     return render(request, template_name, context)
+
+
+@main_app.exception_handler(LoginRequiredException)
+async def login_required_exception_handler(request, exc):
+    return redirect("/login", remove_session=True)
