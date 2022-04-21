66dac4b5ceebcd4d9ca164adb0ecc24405b15e1a
moyosore1 <moyosoreolumideobi@gmail.com>
2022-04-21 13:42:06 -0400

    htmx for video edit

--- app/video/routers.py
+++ app/video/routers.py
@@ -4,6 +4,7 @@ from typing import Optional
 
 from fastapi import APIRouter, Request, Form, Depends
 from fastapi.responses import HTMLResponse
+from starlette.exceptions import HTTPException
 
 
 from app.shortcuts import (redirect, render, get_object_or_404, is_htmx)
@@ -102,6 +103,55 @@ def video_edit_post_view(request: Request, host_id: str, is_htmx=Depends(is_htmx
     }
 
     video = get_object_or_404(Video, host_id=host_id)
+    context = {
+        "host_id": host_id,
+        "video": video
+    }
     data, errors = utils.valid_schema_data_or_error(raw_data, VideoEditSchema)
+    if len(errors) > 0:
+        return render(request, "videos/edit.html", context, status_code=400)
     video.title = data.get('title') or video.title
     video.update_video_url(url, save=True)
+    return render(request, "videos/edit.html", context)
+
+
+@router.get("/hx-edit/{host_id}", response_class=HTMLResponse)
+@login_required
+def video_edit_get_hx_view(request: Request, host_id: str, is_htmx=Depends(is_htmx)):
+    if not is_htmx:
+        raise HTTPException(status_code=400)
+    video =  None
+    not_found = False
+    try:
+        video = get_object_or_404(Video, host_id=host_id)
+    except:
+        not_found = True
+    if not_found:
+        return HTMLResponse("Not found.")
+    context = {
+        "host_id": host_id,
+        "video": video
+    }
+    return render(request, 'videos/htmx/edit.html', context)
+
+
+@router.post("/hx-edit/{host_id}", response_class=HTMLResponse)
+@login_required
+def video_edit_post_hx_view(request: Request, host_id: str, is_htmx=Depends(is_htmx), title: str = Form(...),  url: str = Form(...)):
+    raw_data = {
+        "title": title,
+        "url": url,
+        "user_id": request.user.username
+    }
+
+    video = get_object_or_404(Video, host_id=host_id)
+    context = {
+        "host_id": host_id,
+        "video": video
+    }
+    data, errors = utils.valid_schema_data_or_error(raw_data, VideoEditSchema)
+    if len(errors) > 0:
+        return render(request, "videos/edit.html", context, status_code=400)
+    video.title = data.get('title') or video.title
+    video.update_video_url(url, save=True)
+    return render(request, "videos/edit.html", context)
