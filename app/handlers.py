a0a63ed859c9d27c496ab2a27d1982683fe53d23
moyosore1 <moyosoreolumideobi@gmail.com>
2022-05-03 07:44:15 -0400

    nothing

--- app/handlers.py
+++ app/handlers.py
@@ -9,7 +9,7 @@ from app.users.exceptions import LoginRequiredException
 async def http_exception_handler(request, exc):
     status_code = exc.status_code
     template_name = "error.html"
-    context = {"status_code":status_code}
+    context = {"status_code": status_code}
     return render(request, template_name, context)
 
 
